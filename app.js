b494ab4f597d409ae9bf11e54faddabcf4a49de7
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-26 10:48:08 +0100

    Move review express routes to its own js file.
    Move reviews route code from main/route app.js to a new reviews.js file.
    
    Debug the code to ensure it still works by copying code dependencies from app.js to reviews.js.

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const { reviewSchema } = require('./schemas');
 
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Review = require('./models/review');
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/air-camp', {
   useNewUrlParser: true,
@@ -32,11 +32,12 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use runs on every single route.
+// app.use to run on every single route.
 
 app.use(express.urlencoded({ extended: true })); //allows post req in express via form.
 app.use(methodOverride('_method'));
-app.use("/campgrounds", campgrounds)
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 //custom middleware for validation using Joi
 
@@ -65,32 +66,6 @@ const validateReview = (req, res, next) => {
 app.get('/', (req, res) => {
   res.render('home'); // first link to home ejs
 });
-
-// REVIEW ROUTES
-
-app.post(
-  '/campgrounds/:id/reviews',
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review); //note that the method .review links to the form variables we created - review[rating|body]
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.delete(
-  '/campgrounds/:id/reviews/:reviewId',
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    // Pull operator from mongodDB - see docs for further deets.
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-  })
-);
 
 // app.all is for every single request and * is for all paths.
 app.all('*', (req, res, next) => {
---
+++ routes/reviews.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router({mergeParams: true}); //mergeParams is very important to allow the params.id info to be shared when setting app.use with :id e.g. '/campgrounds/:id/reviews'
+const catchAsync = require('../utils/catchAsync');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const ExpressError = require('../utils/ExpressError');
+const { reviewSchema } = require('../schemas');
+
+// MIDDLEWARE
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// REVIEW ROUTES
+
+router.post(
+  '/',
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review); //note that the method .review links to the form variables we created - review[rating|body]
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  '/:reviewId',
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    // Pull operator from mongodDB - see docs for further deets.
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+module.exports = router;
