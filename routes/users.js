5da5bd933627daa34de41698966182ac2f8adca2
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-28 11:14:31 +0100

    Add code to allow users to register in users.js
    Users now should be able to register on the app.
    
    Add try-catch and error middleware in case of any errors.
    Ensure app can pass basic tests.

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-  res.send(req.body);
-});
+// TRY-CATCH FOR USER EXPERIENCE - BETTER TO FLASH AND REDIRECT TO REGISTER PAGE IF SOMETHING WENT WRONG e.g. duplicate username.
+router.post(
+  '/register',
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash('success', 'Welcome to AirCamp!');
+      res.redirect('/campgrounds');
+    } catch (e) {
+      req.flash('error', e.message);
+      res.redirect('register');
+    }
+  })
+);
 
 module.exports = router;
