caff7afa2bd85909f3a2b541787695b0d5150fbb
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-28 15:15:40 +0100

    Add code to keep user loggedin post reg.
    Added code in the users.js to keep users loggedin after succesfully
    registering.

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,17 @@ router.get('/register', (req, res) => {
 // TRY-CATCH FOR USER EXPERIENCE - BETTER TO FLASH AND REDIRECT TO REGISTER PAGE IF SOMETHING WENT WRONG e.g. duplicate username.
 router.post(
   '/register',
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash('success', 'Welcome to AirCamp!');
-      res.redirect('/campgrounds');
+      req.login(registeredUser, (err) => {
+        //keeps the user loggedin after successful registration.
+        if (err) return next(err);
+        req.flash('success', 'Welcome to AirCamp!');
+        res.redirect('/campgrounds');
+      });
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('register');
@@ -46,7 +50,7 @@ router.post(
 
 router.get('/logout', (req, res) => {
   req.logout();
-  req.flash('success', "Logged out succesfully")
+  req.flash('success', 'Logged out succesfully');
   res.redirect('/campgrounds');
 });
 
