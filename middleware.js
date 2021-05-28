7e291d3459829a38fefceb83ba8f3cd10d704105
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-28 15:39:34 +0100

    Users to be redirected to page access before login
    Add code so that users will be returned to the page they were trying
    to access when they were asked to login.
    
    This logic was implemented using sessions.

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl; // store URL they are requesting.
     req.flash('error', 'You must be signed in first');
     return res.redirect('/login');
   }
