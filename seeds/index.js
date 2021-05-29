01b32910c24a8beb9900c5adb1c07e0d7358d96f
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-29 10:36:09 +0100

    Link newly created campground with registered user
    Update campground schema to incorporate an author id.
    Display author on campground show page
    link reg user with newly created campground.

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '60b0c05ef62483e72ebd0fcd',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
