2a9ca2d4b1814b85a7892327c8648059c2d87264
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-03 11:21:53 +0100

    Update seedDB to include ukCities and reseed
    Also update the default pictures to make it more consistent with
    ukCities.

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 //This runs separetely from our node app.
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const { ukCities } = require('./ukCities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
@@ -16,29 +17,31 @@ db.once('open', () => {
   console.log('Database connected');
 });
 
+console.log(ukCities[0].city);
+
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
+    const random443 = Math.floor(Math.random() * 443); //there are 443 UKcities in the JSON obj
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: '60b0c05ef62483e72ebd0fcd',
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      location: `${ukCities[random443].city}`, //${cities[random1000].state}
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem nemo quod minus molestiae, architecto numquam rerum voluptatibus, blanditiis, perspiciatis repellat eum ducimus error. Quos nesciunt recusandae maiores eligendi sunt.',
       price: price,
-      geometry: { type: 'Point', coordinates: [-2.991665, 53.407154] },
+      geometry: { type: 'Point', coordinates: [ukCities[random443].lng, ukCities[random443].lat] },
       images: [
         {
-          url: 'https://res.cloudinary.com/fabien14/image/upload/v1622547197/AirCamp/jyqlogolkhpmuzgn1yrl.jpg',
-          filename: 'AirCamp/jyqlogolkhpmuzgn1yrl',
+          url: 'https://res.cloudinary.com/fabien14/image/upload/v1622715244/AirCamp/shell-campingwithstyle-1bSqus7DHro-unsplash_hywyps.jpg',
+          filename: 'shell-campingwithstyle-1bSqus7DHro-unsplash_hywyps',
         },
         {
-          url: 'https://res.cloudinary.com/fabien14/image/upload/v1622547197/AirCamp/ztuqduyh4mkkidubwsxy.jpg',
-          filename: 'AirCamp/ztuqduyh4mkkidubwsxy',
+          url: 'https://res.cloudinary.com/fabien14/image/upload/v1622715244/AirCamp/shell-campingwithstyle-J8tD4HLJLSw-unsplash_z1svqk.jpg',
+          filename: 'shell-campingwithstyle-J8tD4HLJLSw-unsplash_z1svqk',
         },
       ],
     });
