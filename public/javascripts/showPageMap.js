bb8ddc20216474b56c089a83d2c6846f72f3f5c4
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-05 10:56:18 +0100

    install sanitize-html npm
    Require to protect against XSS attacks
    Update Joi with an extension that allows client side validation to work
    with the sanitize-html npm.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -12,3 +12,5 @@ new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
   .addTo(map);
+
+  
