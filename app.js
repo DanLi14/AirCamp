6e68dac24b847828f0dec58b5fac4035ae46b4e0
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-06 11:21:57 +0100

    fix typo on app.js relating to env.

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-const port = proceess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`connected to port: ${port}`);
 });
