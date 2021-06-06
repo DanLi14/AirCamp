089c08830f8dc3280c2881a643b23da569e6b91a
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-06 16:08:20 +0100

    Updated port to envURL post local development.

--- app.js
+++ app.js
@@ -178,9 +178,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-// process.env.PORT || 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`connected to port: ${port}`);
 });
-
