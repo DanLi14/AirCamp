b6d227108fdb01d78bc86d01f1591cdf8ec82c60
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-11 15:10:18 +0100

    turn on secure: true for sessionConfig.

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    // secure: true, // turn on when deployed.
+    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //equates to 1 week
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
